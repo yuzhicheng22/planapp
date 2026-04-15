14fe0fc19d0c03d1c7b6c8b8b6291bf718736ad6
yuzhicheng <yuzcyuzc22@163.com>
2026-04-15 19:36:32 +0800

    fix:修复错误属性

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="zh-CN"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col overflow-hidden">
+      <body className="min-h-full flex flex-col">
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
